bea9c9f0239097f5601d86e263ab4692f066b2d1
Phil Hunt <cabalamat@gmail.com>
2019-04-18 04:35:37 +0100

    tornado

--- app/server.py
+++ app/server.py
@@ -12,9 +12,7 @@ if __name__ == "__main__":
     container = wsgi.WSGIContainer(app)
     http_server = httpserver.HTTPServer(container)
     
-    ##### PH changes 2-Feb-2019
-    http_server.bind(8300)
-    http_server.start(8300)
+    http_server.listen(80)
     
     ioloop.IOLoop.instance().start()
 
